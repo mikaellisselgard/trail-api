bbcbad69ed36bf6f396cd510e3d886e8bd864d12
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-09-13 21:58:52 +0200

    Move RSpec configuration to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'rspec/rails'
-require 'support/factory_girl'
 require 'support/database_cleaner'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
 end
