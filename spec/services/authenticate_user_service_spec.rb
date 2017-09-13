8558fcc9aa225811e4129088a41c74e93dd20349
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-09-13 21:58:27 +0200

    Require spec_helper

--- spec/services/authenticate_user_service_spec.rb
+++ spec/services/authenticate_user_service_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 describe AuthenticateUserService, type: :service do
   subject(:service) { described_class }
 
--- spec/services/authorize_api_request_service_spec.rb
+++ spec/services/authorize_api_request_service_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 describe AuthorizeAPIRequestService, type: :service do
   subject(:service) { described_class }
   let!(:user) { create(:user) }
