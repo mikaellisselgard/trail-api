5fdfc14377eba6a5d1c1d6b08372a5ff1f1ae9fe
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-09-25 20:52:43 +0200

    Add a couple of species to fish

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Fish < ApplicationRecord
+  SPECIES = %w[PIKE PERCH ZANDER].freeze
+
   has_many :locatable_items, as: :locatable
   has_many :locations, through: :locatable_items
+
+  validates :species, inclusion: SPECIES
 end
