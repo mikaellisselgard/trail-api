57435c06ca8207cc821917c8d6a2c4379b9e4b18
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-10-02 20:33:14 +0200

    Add fish and mushroom seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 User.create(
-  [
-    {
-      name: 'Demo User',
-      email: 'demo_user@example.com',
-      password: 'password'
-    }
-  ]
+  name: 'Demo User',
+  email: 'demo_user@example.com',
+  password: 'password'
 )
+
+Fish.create(species: 'PIKE')
+Fish.create(species: 'PERCH')
+Fish.create(species: 'ZANDER')
+
+Mushroom.create(species: 'CHANTARELLE')
+Mushroom.create(species: 'TRUFFLE')
+Mushroom.create(species: 'AGARIC')
